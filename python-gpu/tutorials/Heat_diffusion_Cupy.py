90458bfc506c08c6af72aaa076c1fc3a71e03ce9
VenkiPhy6 <venkar96@gmail.com>
2025-08-08 16:09:26 +0000

    edited cp

--- python-gpu/tutorials/Heat_diffusion_Cupy.py
+++ python-gpu/tutorials/Heat_diffusion_Cupy.py
@@ -16,9 +16,18 @@ f[N//2, N//2] = 10000  # Heat source
 start = time.time()
 # Jacobi iteration
 for iteration in range(max_iters):
-####################################
-#######Insert your code here########
-####################################
+    ####################################
+    #######Insert your code here########
+    ####################################
+    
+    p_new = p.copy()
+    p_new[1:-1, 1:-1] = 0.25 * (
+        p[:-2, 1:-1] + p[2:, 1:-1] +
+        p[1:-1, :-2] + p[1:-1, 2:] +
+        dx**2 * f[1:-1, 1:-1]
+    )
+    diff = cp.linalg.norm(p_new - p)
+    p = p_new
 
     # Optional: bring scalar to CPU to check for convergence
     if diff.get() < tolerance:
